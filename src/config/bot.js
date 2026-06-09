af60a8af6de19c5d61b258a8026f3521c952d3d3
Lenysha_16 <lenysharamjuthan@gmail.com>
2026-06-09 10:10:05 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -89,7 +89,7 @@ export const botConfig = {
     colors: {
       // Main brand colors.
       primary: "#336699", 
-      secondary: "#2F3136", 
+      secondary: "#800000", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
